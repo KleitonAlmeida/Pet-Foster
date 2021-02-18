70b6f30621ce411d7c8364762895ba5da19c5ac9
Kleiton Almeida <76787198+KleitonAlmeida@users.noreply.github.com>
2021-02-17 22:10:59 -0300

    Create posicao.dart

---
+++ lib/pages/posicao.dart
@@ -0,0 +1,118 @@
+import 'dart:math';
+
+/// Flutter code sample for AppBar
+
+// This sample shows an [AppBar] with two simple actions. The first action
+// opens a [SnackBar], while the second action navigates to a new page.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatelessWidget(),
+    );
+  }
+}
+
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+final SnackBar snackBar = const SnackBar(content: Text('Showing Snackbar'));
+
+void openPage(BuildContext context) {
+  Navigator.push(context, MaterialPageRoute(
+    builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Next page'),
+        ),
+        body: const Center(
+          child: Text(
+            'This is the next page',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      );
+    },
+  ));
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Pets Mais Adotados'),
+        actions: <Widget>[
+         IconButton(
+                icon: Icon(Icons.favorite_border),color: Colors.orange, 
+                onPressed: () {}
+            
+          ),
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Next page',
+            onPressed: () {
+              openPage(context);
+            },
+          ),
+        ],
+       
+
+      ),
+       body: criaTabela(),
+    );
+  }
+
+
+
+criaTabela() {
+    return Table(
+      defaultColumnWidth: FixedColumnWidth(150.0),
+      border: TableBorder(
+        horizontalInside: BorderSide(
+          color: Colors.black,
+          style: BorderStyle.solid,
+          width: 1.0,
+        ),
+        verticalInside: BorderSide(
+          color: Colors.black,
+          style: BorderStyle.solid,
+          width: 1.0,
+        ),
+      ),
+      children: [
+        _criarLinhaTable("Posicao, Animal, Sairam"),
+        _criarLinhaTable("01, Dog, 16 "),
+        _criarLinhaTable("02, Gato, 9"),
+        _criarLinhaTable("03, Aves, 6"),
+        _criarLinhaTable("04, Outros, 2"),
+      ],
+    );
+  }
+  _criarLinhaTable(String listaNomes) {
+    return TableRow(
+      children: listaNomes.split(',').map((name) {
+        return Container(
+          alignment: Alignment.center,
+          child: Text(
+            name,
+            style: TextStyle(fontSize: 20.0),
+          ),
+          padding: EdgeInsets.all(8.0),
+        );
+      }).toList(),
+    );
+  }
+}
+          
