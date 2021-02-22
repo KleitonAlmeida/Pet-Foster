9c2db29f657db1e5cfaecb229c6308e2fcf86cdd
Kleiton Almeida <76787198+KleitonAlmeida@users.noreply.github.com>
2021-02-22 14:15:10 -0300

    Update and rename lib/pages/Cadastro _Pets.dart to lib/Páginas/Cadastro _Pets.dart

--- lib/Páginas/Cadastro _Pets.dart
+++ lib/Páginas/Cadastro _Pets.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyApp());
-
-
 class MyApp extends StatelessWidget {
-
+  @override
+  Widget build(BuildContext context) {
   final TextEditingController _controladorNome = TextEditingController();
   final TextEditingController _controladorIdade = TextEditingController();
   final TextEditingController _controladorSexo = TextEditingController();
   
- @override
-  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cadastrando o Pet'),
-        ),
-
-         body: Column(
+     
+        home: Scaffold(
+          
+      appBar: AppBar(title: Icon(Icons.navigate_before), 
+backgroundColor: Colors.orange),
+  
+      body: Column(
           children: <Widget>[
             TextField( controller: _controladorNome,),
             TextField( controller: _controladorIdade,),
@@ -33,7 +30,26 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_a_photo),
+          onPressed: () {
+            print("O botão foi pressionado");
+          }),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            Text("Cadastro  "),
+            Icon(Icons.delete),
+            IconButton(
+              icon: Icon(Icons.add_alert),
+              onPressed: () {
+                print("O IconButton foi pressionado");
+              }
+              ,)
+          ],
+        ),
+        color: Colors.blueAccent,
       ),
-    );
+    ));
   }
 }
